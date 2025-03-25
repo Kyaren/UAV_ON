bb6f17083c94de354693b156f5efb035cb79173f
Kyaren <104709189+Kyaren@users.noreply.github.com>
2025-03-25 15:02:23 +0800

    model

--- src/perception.py
+++ src/perception.py
@@ -3,7 +3,7 @@ from airsim_plugin.AirVLNSimulatorClientTool import AirVLNSimulatorClientTool
 
 class simState:
     def __init__(self, ):
-
+        pass
 
 class DronePerception:
     def __init__(self, ):
@@ -13,4 +13,18 @@ class DronePerception:
     def getObservation(self,):
         pass
 
-    def 
+    def getSearchHeight(self,):
+        """
+            Get the height of the drone during search
+
+            return (float): height of the drone
+        """
+
+    def isTargetDetected(self,) -> bool:
+        """
+            Check if the target is detected in the current observation
+
+            return (bool): True if the target is detected, False otherwise
+        """
+
+     
